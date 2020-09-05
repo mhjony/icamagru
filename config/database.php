02ce1176d0fcd001b6744a8f64c493ce50449abf
mahmudulhasanjhoony <mahmudul.jhony@gmail.com>
2020-09-05 22:39:40 +0300

    database.php change

--- config/database.php
+++ config/database.php
@@ -1,5 +1,10 @@
 <?php
-$DB_DSN = "localhost";
-$DB_USER = "root";
-$DB_PASSWORD = "123456";
+// $DB_DSN = "localhost";
+// $DB_USER = "root";
+// $DB_PASSWORD = "123456";
+// $DB_NAME = "camagru";
+
+$DB_DSN = "https://test-camagru.herokuapp.com/";
+$DB_USER = "ba720a4856594e";
+$DB_PASSWORD = "91a05ca5";
 $DB_NAME = "camagru";
