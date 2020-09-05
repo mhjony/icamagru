452f486302f99acb73aeb23daa8ad6018d990a6b
mahmudulhasanjhoony <mahmudul.jhony@gmail.com>
2020-09-06 00:03:46 +0300

    connection issue

--- admin/dbConnectionNew.php
+++ admin/dbConnectionNew.php
@@ -1,9 +1,9 @@
 <?php
 
-$DB_DSN = "mysql:dbname=camagru;host=127.0.0.1";
-$DB_USER = "root";
-$DB_PASSWORD = "123456";
-$DB_DSN_NO_DB = "mysql:host=127.0.0.1";
+$DB_DSN = "mysql:dbname=heroku_b52e11e37479cf3;host=eu-cdbr-west-03.cleardb.net";
+$DB_USER = "ba720a4856594e";
+$DB_PASSWORD = "91a05ca5";
+$DB_DSN_NO_DB = "mysql:host=eu-cdbr-west-03.cleardb.net";
 
 try
 {
