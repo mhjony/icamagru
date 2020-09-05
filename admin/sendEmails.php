89aa33162bf9f63ede114981d7caf3789ce37c9b
mahmudulhasanjhoony <mahmudul.jhony@gmail.com>
2020-09-06 00:37:17 +0300

    gmail smtp testing

--- admin/sendEmails.php
+++ admin/sendEmails.php
@@ -2,21 +2,50 @@
 
 function sendVerificationEmail($email, $token)
 {
-    echo $email;
-    echo $token;
     $body = '
         Thanks for sign up in our application
         Click the following link in order to complete your signup process:
       http://localhost:8080/camagru/admin/verificationEmail.php?token=' . $token . '';
-    $headers = "From: Admin\r\n";
+    // $headers = "From: Admin\r\n";
     // Send the message
-    if (mail($email, "Verify your email", $body, $headers)) {
-        echo '<script type="text/JavaScript">  
-        alert("A confirmation email has been sent to your email"); 
-        </script>';
+    // if (mail($email, "Verify your email", $body, $headers)) {
+    //     echo '<script type="text/JavaScript">  
+    //     alert("A confirmation email has been sent to your email"); 
+    //     </script>';
+    // } else {
+    //     echo "Message Error";
+    // }
+    require_once "Mail.php";
+
+    $from = 'mahmudul.jhony@gmail.com'; //change this to your email address
+    $to = $email; // change to address
+    $subject = 'Insert subject here'; // subject of mail
+    // $body = "Hello world! this is the content of the email"; //content of mail
+
+    $headers = array(
+        'From' => $from,
+        'To' => $to,
+        'Subject' => $subject
+    );
+
+    $smtp = Mail::factory('smtp', array(
+            'host' => 'smtp.gmail.com',
+            'port' => 465,
+            'auth' => true,
+            'username' => 'jhony.mbstu@gmail.com', //your gmail account
+            'password' => '01722904691' // your password
+        ));
+
+    // Send the mail
+    $mail = $smtp->send($to, $headers, $body);
+
+    //check mail sent or not
+    if (PEAR::isError($mail)) {
+        echo '<p>'.$mail->getMessage().'</p>';
     } else {
-        echo "Message Error";
+        echo '<p>Message successfully sent!</p>';
     }
+
 }
 
 
